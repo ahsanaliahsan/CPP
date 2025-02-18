bac5c9cb364276045f8f52a489677f5631bc9759
Muhammad Ali Ahsan <aliahsanali@gmail.com>
2025-02-18 15:49:34 +0500

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
+#include <string>
 #include <thread>
 #include <chrono>
 
 // Simple function to be run in a thread
-void count_numbers(int id, int max) {
+void count_numbers(char threadname, int max) {
     for(int i = 1; i <= max; ++i) {
-        std::cout << "Thread " << id << " counting: " << i << std::endl;
+        std::cout << " ID: " << std::this_thread::get_id() << " Name: " << threadname << " counting: " << i << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
 int main() {
     // Create two threads
-    std::thread t1(count_numbers, 1, 5);
-    std::thread t2(count_numbers, 2, 5);
+    std::thread t1(count_numbers, 'A', 15);
+    std::thread t2(count_numbers, 'B', 5);
 
     // Main thread message
     std::cout << "Main thread: Waiting for threads to finish..." << std::endl;
